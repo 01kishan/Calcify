4f7e64c8e2d2b426eef4d9d6189795a31a40736e
kishan <kkshakya1999@gmail.com>
2025-10-06 01:45:24 +0530

    UI fix

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,46 +2,7 @@ import { useState } from 'react';
 import '../styles/Calculator.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-
-function calculateRes(input) {
-  try {
-    // Split numbers and operators
-    const numbers = input.split(/[\+\-\*\/]/).map(Number);
-    const operators = input.replace(/[0-9.]/g, "").split("");
-
-    let result = numbers[0];
-
-    if( operators.length == 0 )
-      return input;
-
-    for (let i = 0; i < operators.length; i++) {
-      const operator = operators[i];
-      const nextNum = numbers[i + 1];
-
-      switch (operator) {
-        case "+":
-          result += nextNum;
-          break;
-        case "-":
-          result -= nextNum;
-          break;
-        case "*":
-          result *= nextNum;
-          break;
-        case "/":
-          result /= nextNum;
-          break;
-        default:
-          break;
-      }
-    }
-
-    return result;
-  } catch (error) {
-    return "Error";
-  }
-}
-
+import {calculateRes} from '../utility/calcualtions';
 
 const Calculator = () => {
     const [dispayText, setDispayText] = useState("0");
